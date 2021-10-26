6d2e4d335fc2f198b64e69bc3f2aaf8e3d81d1cd
FidelCC <fidelcaicedo@comfama.com.co>
2021-10-25 22:10:40 -0500

    updated: Uploaded Procfile and runtime.txt

--- fastapi_arduino.py
+++ fastapi_arduino.py
@@ -73,4 +73,4 @@ async def get_temperature():
 if __name__ == "__main__":
 
     uvicorn.run("fastapi_arduino:app", host="127.0.0.1",
-                port=4000, log_level="info")
+                port=5000, log_level="info")
